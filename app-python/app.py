5f2b74964bd93d543175137c9967aaf7545f1a67
Yuliet Chavarria <yuchag94@gmail.com>
2025-05-08 14:58:55 -0500

    Creacion de gestion de vehiculos

--- app-python/app.py
+++ app-python/app.py
@@ -9,13 +9,90 @@ config = {
     'host': 'tramway.proxy.rlwy.net',
     'user': 'root',
     'password': 'yLaNXNWGkUGayEvHDtQEmdOjfvLcRqpL',
-    'database': 'railway',
+    'database': 'gestion_vehicular',
     'port': 59274
 }
+
+# HOME
 
 @app.route('/')
 def home():
     return '¡API Flask funcionando correctamente en Render!'
+
+# VEHICULO
+
+app.route('/vehiculo', methods=['POST'])
+def agregar_vehiculo():
+    data = request.json
+    cursor = conexion.cursor()
+    cursor.execute("""
+        INSERT INTO vehiculo (tarjeta_propiedad, soat, poliza, revision, gps, clave_gps)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    """, (data['tarjeta'], data['soat'], data['poliza'], data['revision'], data['gps'], data['clave_gps']))
+    conexion.commit()
+    return jsonify({"mensaje": "Vehículo agregado"}), 201
+
+# PROPIETARIO
+
+@app.route('/propietario', methods=['POST'])
+def agregar_propietario():
+    data = request.json
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        query = """
+            INSERT INTO propietario (cedula, rut, direccion, municipio, cuenta_bancaria, correo)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """
+        values = (
+            data['cedula'],
+            data['rut'],
+            data['direccion'],
+            data['municipio'],
+            data['cuenta_bancaria'],
+            data['correo']
+        )
+        cursor.execute(query, values)
+        connection.commit()
+        return jsonify({"mensaje": "Propietario agregado correctamente"}), 201
+    except Error as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+# CONDUCTOR 
+
+@app.route('/conductor', methods=['POST'])
+def agregar_conductor():
+    data = request.json
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        query = """
+            INSERT INTO conductor (cedula, direccion, municipio, licencia, correo, cuenta_bancaria, foto1, foto2)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        values = (
+            data['cedula'],
+            data['direccion'],
+            data['municipio'],
+            data['licencia'],
+            data['correo'],
+            data['cuenta_bancaria'],
+            data['foto1'],
+            data['foto2']
+        )
+        cursor.execute(query, values)
+        connection.commit()
+        return jsonify({"mensaje": "Conductor agregado correctamente"}), 201
+    except Error as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
 
 @app.route('/alumnos')
 def obtener_alumnos():
