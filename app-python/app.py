a13a87d7982fe41364266819855a63b644f64e57
Yuliet Chavarria <yuchag94@gmail.com>
2025-05-08 11:30:15 -0500

    Configuracion de variables en raiwlay

--- app-python/app.py
+++ app-python/app.py
@@ -1,32 +1,34 @@
 from flask import Flask, jsonify
 import mysql.connector
+import os
 
 app = Flask(__name__)
 
-# Configuración de conexión a MySQL
-db_config = {
-    'host': 'localhost',           # Cambia si tu base de datos está en otro servidor
-    'user': 'tu_usuario',
-    'password': 'tu_contraseña',
-    'database': 'nombre_base_datos'
+# Configura la conexión usando las variables de Railway
+config = {
+    'host': 'mysql.railway.internal',
+    'user': 'root',
+    'password': 'yLaNXNWGkUGayEvHDtQEmdOjfvLcRqpL',
+    'database': 'railway',
+    'port': 3306  # o el valor de TU_MYSQLPORT si es distinto
 }
 
 @app.route('/')
-def inicio():
+def home():
     return '¡API Flask funcionando correctamente en Render!'
 
-@app.route('/alumnos', methods=['GET'])
+@app.route('/alumnos')
 def obtener_alumnos():
     try:
-        conexion = mysql.connector.connect(**db_config)
-        cursor = conexion.cursor(dictionary=True)
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor(dictionary=True)
         cursor.execute("SELECT * FROM alumnos")
         resultados = cursor.fetchall()
         cursor.close()
-        conexion.close()
+        connection.close()
         return jsonify(resultados)
-    except mysql.connector.Error as error:
-        return jsonify({'error': str(error)}), 500
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(debug=False, host='0.0.0.0', port=10000)
