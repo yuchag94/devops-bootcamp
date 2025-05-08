e80eda8c1d014c079167ac744a98c5d92d785def
Yuliet Chavarria <yuchag94@gmail.com>
2025-05-08 18:16:06 -0500

    Front y Back v2

--- app-python/app.py
+++ app-python/app.py
@@ -46,4 +46,4 @@ def listar_vehiculos():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
